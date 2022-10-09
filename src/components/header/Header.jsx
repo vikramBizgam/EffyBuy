6b96f5f1cb77cd3e0e5ffd7a8655a66eba4adfba
vikram suryawanshi <vikram.suryawanshi619.vs@gmail.com>
2022-10-09 20:54:14 +0530

    checking link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
       >
         <div className="d-flex align-items-center justify-content-between">
           <div className="logo">
-            <Link to="/">
+            <Link to="/EffyBuy">
               <img src={effybuy_logo_with_name} alt="EffyBuy Logo" />
             </Link>
           </div>
